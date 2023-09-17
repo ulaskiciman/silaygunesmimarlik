0b60a27b8f6a279c16500379691e95fce90514e0
ulaskiciman <145257746+ulaskiciman@users.noreply.github.com>
2023-09-17 13:17:00 +0300

    Add files via upload

---
+++ sılayteyzeproje/Yeni klasör (7)/src/App.jsx
@@ -0,0 +1,133 @@
+import "./styles.css";
+import Fade from "react-reveal";
+
+function App() {
+  return (
+    <div>
+      <div
+        className="first"
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          outline: "1px solid black"
+        }}
+      >
+        <img
+          className="logo"
+          style={{ height: "100px" }}
+          src="https://cdn.logo.com/hotlink-ok/logo-social.png"
+          alt=""
+        />
+        <h1>SILAYGÜNEŞ</h1>
+        <h5 className="mimarlıktext">MİMARLIK</h5>
+      </div>
+      <div className="işfotoları">
+        <div className="sol">
+          <Fade bottom duration={1500}>
+            <img
+              style={{ height: "600px", width: "400px" }}
+              src="https://media.architecturaldigest.com/photos/56ba787ca254b168296a8fff/16:9/w_5200,h_2924,c_limit/zaha-hadid-architecture-01.jpg"
+              alt=""
+            />
+            <img
+              style={{ height: "400px", width: "400px" }}
+              src="https://media.architecturaldigest.com/photos/56ba787ca254b168296a8fff/16:9/w_5200,h_2924,c_limit/zaha-hadid-architecture-01.jpg"
+              alt=""
+            />
+            <img
+              style={{ height: "300px", width: "400px" }}
+              src="https://media.architecturaldigest.com/photos/56ba787ca254b168296a8fff/16:9/w_5200,h_2924,c_limit/zaha-hadid-architecture-01.jpg"
+              alt=""
+            />
+            <img
+              style={{ height: "500px", width: "400px" }}
+              src="https://media.architecturaldigest.com/photos/56ba787ca254b168296a8fff/16:9/w_5200,h_2924,c_limit/zaha-hadid-architecture-01.jpg"
+              alt=""
+            />
+            <img
+              style={{ height: "400px", width: "400px" }}
+              src="https://media.architecturaldigest.com/photos/56ba787ca254b168296a8fff/16:9/w_5200,h_2924,c_limit/zaha-hadid-architecture-01.jpg"
+              alt=""
+            />
+            <img
+              style={{ height: "300px", width: "400px" }}
+              src="https://media.architecturaldigest.com/photos/56ba787ca254b168296a8fff/16:9/w_5200,h_2924,c_limit/zaha-hadid-architecture-01.jpg"
+              alt=""
+            />
+          </Fade>
+        </div>
+        <div className="orta">
+          <Fade bottom duration={1500}>
+            <img
+              style={{ height: "300px", width: "500px" }}
+              src="https://img.freepik.com/premium-photo/luxury-house-architecture-drawing-sketch-plan-blueprint_691560-6242.jpg?w=2000"
+              alt=""
+            />
+            <img
+              style={{ height: "500px", width: "500px" }}
+              src="https://img.freepik.com/premium-photo/luxury-house-architecture-drawing-sketch-plan-blueprint_691560-6242.jpg?w=2000"
+              alt=""
+            />
+            <img
+              style={{ height: "400px", width: "500px" }}
+              src="https://img.freepik.com/premium-photo/luxury-house-architecture-drawing-sketch-plan-blueprint_691560-6242.jpg?w=2000"
+              alt=""
+            />
+            <img
+              style={{ height: "600px", width: "500px" }}
+              src="https://img.freepik.com/premium-photo/luxury-house-architecture-drawing-sketch-plan-blueprint_691560-6242.jpg?w=2000"
+              alt=""
+            />
+            <img
+              style={{ height: "200px", width: "500px" }}
+              src="https://img.freepik.com/premium-photo/luxury-house-architecture-drawing-sketch-plan-blueprint_691560-6242.jpg?w=2000"
+              alt=""
+            />
+            <img
+              style={{ height: "600px", width: "500px" }}
+              src="https://img.freepik.com/premium-photo/luxury-house-architecture-drawing-sketch-plan-blueprint_691560-6242.jpg?w=2000"
+              alt=""
+            />
+          </Fade>
+        </div>
+        <div className="sağ">
+          <Fade bottom duration={1500}>
+            <img
+              style={{ height: "200px", width: "400px" }}
+              src="https://images.unsplash.com/photo-1519662978799-2f05096d3636?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXJjaGl0ZWN0dXJhbHxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80"
+              alt=""
+            />
+            <img
+              style={{ height: "600px", width: "400px" }}
+              src="https://images.unsplash.com/photo-1519662978799-2f05096d3636?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXJjaGl0ZWN0dXJhbHxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80"
+              alt=""
+            />
+            <img
+              style={{ height: "400px", width: "400px" }}
+              src="https://images.unsplash.com/photo-1519662978799-2f05096d3636?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXJjaGl0ZWN0dXJhbHxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80"
+              alt=""
+            />
+            <img
+              style={{ height: "500px", width: "400px" }}
+              src="https://images.unsplash.com/photo-1519662978799-2f05096d3636?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXJjaGl0ZWN0dXJhbHxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80"
+              alt=""
+            />
+            <img
+              style={{ height: "200px", width: "400px" }}
+              src="https://images.unsplash.com/photo-1519662978799-2f05096d3636?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXJjaGl0ZWN0dXJhbHxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80"
+              alt=""
+            />
+            <img
+              style={{ height: "500px", width: "400px" }}
+              src="https://images.unsplash.com/photo-1519662978799-2f05096d3636?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXJjaGl0ZWN0dXJhbHxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80"
+              alt=""
+            />
+          </Fade>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
